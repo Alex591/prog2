c9bb5cba8476d4ef21ccb9f1262751d289737dc2
Alex591 <adamalex200090@yahoo.com>
2020-11-21 16:17:39 +0200

    ini

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'designerNOdeND.ui'
+## Form generated from reading UI file 'MainUIOSFPXP.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.14.1
 ##
@@ -16,60 +16,64 @@ from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
 from PySide2.QtWidgets import *
 
 
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        if MainWindow.objectName():
-            MainWindow.setObjectName(u"MainWindow")
-        MainWindow.setWindowModality(Qt.NonModal)
-        MainWindow.resize(770, 239)
-        MainWindow.setToolButtonStyle(Qt.ToolButtonIconOnly)
-        MainWindow.setTabShape(QTabWidget.Rounded)
-        MainWindow.setUnifiedTitleAndToolBarOnMac(False)
-        self.centralwidget = QWidget(MainWindow)
+class Ui_Lottozomasina(object):
+    def setupUi(self, Lottozomasina):
+        if Lottozomasina.objectName():
+            Lottozomasina.setObjectName(u"Lottozomasina")
+        Lottozomasina.resize(643, 392)
+        Lottozomasina.setAcceptDrops(True)
+        Lottozomasina.setDocumentMode(False)
+        Lottozomasina.setDockNestingEnabled(False)
+        Lottozomasina.setUnifiedTitleAndToolBarOnMac(False)
+        self.actionInform_ci = QAction(Lottozomasina)
+        self.actionInform_ci.setObjectName(u"actionInform_ci")
+        self.centralwidget = QWidget(Lottozomasina)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.skandinav = QPushButton(self.centralwidget)
-        self.skandinav.setObjectName(u"skandinav")
-        self.skandinav.setGeometry(QRect(120, 110, 191, 81))
+        self.pushButton = QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(90, 320, 191, 41))
         self.pushButton_2 = QPushButton(self.centralwidget)
         self.pushButton_2.setObjectName(u"pushButton_2")
-        self.pushButton_2.setGeometry(QRect(400, 110, 201, 81))
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.statusbar = QStatusBar(MainWindow)
-        self.statusbar.setObjectName(u"statusbar")
-        MainWindow.setStatusBar(self.statusbar)
+        self.pushButton_2.setGeometry(QRect(340, 320, 191, 41))
+        self.welcomeLabel = QLabel(self.centralwidget)
+        self.welcomeLabel.setObjectName(u"welcomeLabel")
+        self.welcomeLabel.setGeometry(QRect(180, 290, 263, 13))
+        self.photoLabel = QLabel(self.centralwidget)
+        self.photoLabel.setObjectName(u"photoLabel")
+        self.photoLabel.setGeometry(QRect(100, 10, 441, 261))
+        self.photoLabel.setPixmap(QPixmap(u"photos/Logo_full.png"))
+        self.photoLabel.setScaledContents(True)
+        Lottozomasina.setCentralWidget(self.centralwidget)
+        self.menuBar = QMenuBar(Lottozomasina)
+        self.menuBar.setObjectName(u"menuBar")
+        self.menuBar.setGeometry(QRect(0, 0, 643, 21))
+        self.menuSegits_g = QMenu(self.menuBar)
+        self.menuSegits_g.setObjectName(u"menuSegits_g")
+        Lottozomasina.setMenuBar(self.menuBar)
 
+        self.menuBar.addAction(self.menuSegits_g.menuAction())
+        self.menuSegits_g.addAction(self.actionInform_ci)
 
-        self.retranslateUi(MainWindow)
+        self.retranslateUi(Lottozomasina)
 
-        QMetaObject.connectSlotsByName(MainWindow)
-
-        self.skandinav.clicked.connect(self.show_popup)
-
-
-
-
+        QMetaObject.connectSlotsByName(Lottozomasina)
     # setupUi
 
-    def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.skandinav.setText(QCoreApplication.translate("MainWindow", u"SKANDIN\u00c1V", None))
-        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"\u00d6T\u00d6SLOTT\u00d3", None))
+    def retranslateUi(self, Lottozomasina):
+        Lottozomasina.setWindowTitle(QCoreApplication.translate("Lottozomasina", u"Szerencs\u00e9tlenj\u00e1t\u00e9k Zrt. Lott\u00f3z\u00f3masina", None))
+        self.actionInform_ci.setText(QCoreApplication.translate("Lottozomasina", u"Inform\u00e1ci\u00f3", None))
+        self.pushButton.setText(QCoreApplication.translate("Lottozomasina", u"\u00d6t\u00f6slott\u00f3", None))
+        self.pushButton_2.setText(QCoreApplication.translate("Lottozomasina", u"Skandin\u00e1v Lott\u00f3", None))
+        self.welcomeLabel.setText(QCoreApplication.translate("Lottozomasina", u"\u00dcdv\u00f6z\u00f6lj\u00fck a Szerencs\u00e9tlenj\u00e1t\u00e9k Zrt. Lott\u00f3z\u00f3g\u00e9p\u00e9ben!", None))
+        self.photoLabel.setText("")
+        self.menuSegits_g.setTitle(QCoreApplication.translate("Lottozomasina", u"Segits\u00e9g", None))
     # retranslateUi
 
-    def show_popup(self):
-        print("n")
-        msg=QMessageBox()
-        msg.setWindowTitle("Bitch")
-        msg.setText("kk")
-        msg.setIcon(QMessageBox.Critical)
-        x=msg.exec_()
-
-if __name__== '__main__':
+if __name__ == "__main__":
     import sys
-
-    app = QApplication()
+    app = QApplication(sys.argv)
     MainWindow = QMainWindow()
-    ui = Ui_MainWindow()
+    ui = Ui_Lottozomasina()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
